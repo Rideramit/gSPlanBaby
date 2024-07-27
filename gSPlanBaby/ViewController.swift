2ef56392d6c843ad1058bb07beaa3a7b576c6d90
Mukesh Behera <mukesh.contractorplus@gmail.com>
2024-07-27 11:38:04 +0530

    master updated with login and home screen feature

--- gSPlanBaby/ViewController.swift
+++ gSPlanBaby/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtUserName: UITextField!
+    @IBOutlet weak var txtPass: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("This is login screen")
     }
 
 
